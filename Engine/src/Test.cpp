e5eec360dfba94c5ef3f853bf7e08afbff24e55e
alfie.education <alfie.education@gmail.com>
2020-12-28 11:43:54 +0300

    [Engine] Initial project structure setup

---
+++ Engine/src/Test.cpp
@@ -0,0 +1,10 @@
+#include "Test.h"
+#include <stdio.h>
+
+namespace Engine {
+		
+	void Print() 
+	{
+		printf("Welcome to Engine\n");
+	}
+}
---
+++ Sandbox/src/Application.cpp
@@ -0,0 +1,12 @@
+
+namespace Engine {
+
+	__declspec(dllimport) void Print();
+
+}
+
+
+void main() 
+{
+	Engine::Print();
+}
