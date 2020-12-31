3c009bbee7b64f2f279eba4c801d4ac5ce2102b1
alfie.education <alfie.education@gmail.com>
2020-12-31 13:21:10 +0300

    [Engine] Apply logging system
    Based on spdlog (please check https://github.com/gabime/spdlog for more
    info)
    spdlog is by the MIT License

--- Engine/src/Engine.h
+++ Engine/src/Engine.h
@@ -3,4 +3,5 @@
 // For use by applications
 
 #include "Engine\Application.h"
+#include "Engine\Log.h"
 #include "Engine\EntryPoint.h"
--- Engine/src/Engine/EntryPoint.h
+++ Engine/src/Engine/EntryPoint.h
@@ -6,6 +6,12 @@ extern Engine::Application* Engine::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Engine::Log::Init();
+	EG_CORE_WARN("Core warn log example");
+	EG_CORE_ERROR("Core error log example");
+	EG_INFO("Common info log example");
+	EG_CRITICAL("Common fatal log example");
+
 	auto app = Engine::CreateApplication();
 	app->Run();
 	delete app;
