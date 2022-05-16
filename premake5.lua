d23cb0e920ac88e847db59a99f7261e24ea78ccb
Alexander Smirnov/Russia RAlexander.smirnovD Lab HMI Team Graphics Part(alexander.smirnov@lge.com) <alexander.smirnov@lge.com>
2022-05-16 19:50:58 +0300

    [Engine] Apply premake
    Premake5 app should be available in /vendor/bin/premake folder
    to download use https://github.com/premake/premake-core

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "Engine"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Engine"
+	location "Engine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest" 
+
+		defines
+		{
+			"EG_PLATFORM_WINDOWS",
+			"EG_BUILD_DLL",
+		}
+
+		postbuildcommands
+		{
+			("{copy} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "EN_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "EN_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "EN_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"Engine/vendor/spdlog/include",
+		"Engine/src"
+	}
+
+	links
+	{
+		"Engine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest" 
+
+		defines
+		{
+			"EG_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "EN_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "EN_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "EN_DIST"
+		optimize "On"
