01e05f82e2907c845a48e0edbe04cd77f9051675
Alexander Smirnov <alfie.education@gmail.com>
2022-06-29 19:10:45 +0300

    [Engine] Apply ImGui event system

--- Engine/src/Engine/Events/KeyEvent.h
+++ Engine/src/Engine/Events/KeyEvent.h
@@ -37,6 +37,22 @@ namespace Engine {
 		int m_RepeatCount;
 	};
 
+	class ENGINE_API KeyTypedEvent : public KeyEvent
+	{
+	public:
+		KeyTypedEvent(int keycode)
+			: KeyEvent(keycode) {}
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyTypedEvent: " << m_KeyCode;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyTyped)
+	};
+
 	class ENGINE_API KeyReleasedEvent : public KeyEvent
 	{
 	public:
--- Engine/src/Engine/ImGui/ImGuiLayer.h
+++ Engine/src/Engine/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Engine/Layer.h"
 
+#include "Engine/Events/KeyEvent.h"
+#include "Engine/Events/MouseEvent.h"
+#include "Engine/Events/ApplicationEvent.h"
+
 namespace Engine {
 
 	class ENGINE_API ImGuiLayer : public Layer
@@ -14,6 +18,15 @@ namespace Engine {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizedEvent(WindowResizeEvent& e);
 	private:
 		float m_Time = 0.0f;
 	};
